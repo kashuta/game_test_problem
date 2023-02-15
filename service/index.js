1a7224e670dab72865f1d1135344cdae91bbd29b
A.Kashuta <a@artka.dev>
2023-02-15 13:51:33 +0300

    feat: add some comments and variant for DB query

--- service/index.js
+++ service/index.js
@@ -15,7 +15,7 @@ class Point {
         this.y = y;
     }
 
-// определим метод distanceTo, который принимает данную точку и возвращает расстояние между ними
+// определим метод distanceTo, который принимает данную точку и возвращает расстояние между ними по теореме Пифагора
     distanceTo(other) {
         const dx = other.x - this.x;
         const dy = other.y - this.y;
@@ -49,11 +49,36 @@ class PlayingField {
 
     //TODO: использовать  SQL запрос BETWEEN для получения объектов в радиусе "на лету"
 
+//    constructor(db) {
+//         this.db = db;
+//     }
+//
+//     async getObjectsWithinRadius(character, radius) {
+//         const minX = character.position.x - radius;
+//         const maxX = character.position.x + radius;
+//         const minY = character.position.y - radius;
+//         const maxY = character.position.y + radius;
+
+    //TODO: вынести в репозиторий и добавить индексы на поля x и y в таблице game_objects
+
+//         const query = `
+//       SELECT * FROM game_objects
+//       WHERE x BETWEEN $1 AND $2 AND y BETWEEN $3 AND $4
+//     `;
+//         const values = [minX, maxX, minY, maxY];
+//         const result = await this.db.query(query, values);
+//         const objects = result.rows.map(row => new GameObject(row.id, row.x, row.y));
+//         return objects.filter(obj => obj.position.distanceTo(character.position) <= radius);
+//     }
+
     // определим метод getObjectsWithinRadius, который принимает радиус и возвращает список всех объектов в радиусе от персонажа
     getObjectsWithinRadius(radius) {
+        // определим массив, в который будем складывать объекты в радиусе
         const objectsWithinRadius = [];
         for (const gameObject of this.gameObjects) {
+            // определим расстояние от персонажа до текущего объекта
             const distance = gameObject.position.distanceTo(this.character.position);
+            // если расстояние меньше радиуса, то добавляем объект в массив
             if (distance <= radius) {
                 objectsWithinRadius.push(gameObject);
             }
@@ -99,3 +124,15 @@ for (const obj of objects) {
     const distance = obj.position.distanceTo(character.position);
     console.log(`Объект с ID:${obj.id} в ${distance.toFixed(2)} единиц от персонажа.`);
 }
+
+
+// TODO: Доделать с Sequelize и Postgres в контейнере
+// для теста с БД
+// const { Client } = require('pg');
+// const db = new Client();
+// await db.connect();
+// const character = new Character(1, 50, 50); // assuming character ID is 1
+// const radius = 30; // assuming radius is in cell units
+// const service = new GameObjectService(db);
+// const objectsWithinRadius = await service.getObjectsWithinRadius(character, radius);
+// console.log(objectsWithinRadius);
